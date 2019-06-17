d747604be3d8e4825de2c9459c13e72d6e06717c
Vito <wuwenhao0327@gmail.com>
2019-06-17 17:41:08 +0800

    add 添加 AspNetCoreRateLimitDemo

---
+++ src/AspNetCoreRateLimitDemo/Controllers/IpRateLimitController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using AspNetCoreRateLimit;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreRateLimitDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class IpRateLimitController : Controller
+    {
+        private readonly IpRateLimitOptions _options;
+        private readonly IIpPolicyStore _ipPolicyStore;
+
+        public IpRateLimitController(IOptions<IpRateLimitOptions> optionsAccessor, IIpPolicyStore ipPolicyStore)
+        {
+            _options = optionsAccessor.Value;
+            _ipPolicyStore = ipPolicyStore;
+        }
+
+        [HttpGet]
+        public async Task<IpRateLimitPolicies> GetAsync()
+        {
+            return await _ipPolicyStore.GetAsync(_options.IpPolicyPrefix);
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            var pol = await _ipPolicyStore.GetAsync(_options.IpPolicyPrefix);
+
+            pol.IpRules.Add(new IpRateLimitPolicy
+            {
+                Ip = "8.8.4.4",
+                Rules = new List<RateLimitRule>(new RateLimitRule[] {
+                new RateLimitRule {
+                    Endpoint = "*:/api/testupdate",
+                    Limit = 100,
+                    Period = "1d" }
+            })
+            });
+
+            await _ipPolicyStore.SetAsync(_options.IpPolicyPrefix, pol);
+        }
+    }
+}
---
+++ src/AspNetCoreRateLimitDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreRateLimitDemo
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await (await CreateWebHostBuilder(args)).RunAsync();
+        }
+
+        public static async Task<IWebHost> CreateWebHostBuilder(string[] args)
+        {
+            var webHost = WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
+            using (var scope = webHost.Services.CreateScope())
+            {
+                // get the IpPolicyStore instance
+                var ipPolicyStore = scope.ServiceProvider.GetRequiredService<IIpPolicyStore>();
+
+                // seed IP data from appsettings
+                await ipPolicyStore.SeedAsync();
+            }
+
+            return webHost;
+        }
+    }
+}
