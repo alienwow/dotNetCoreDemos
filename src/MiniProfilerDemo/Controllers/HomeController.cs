747620ee57112fd09f5ec37aacd0216807a2b87a
Vito <wuwenhao0327@gmail.com>
2019-06-18 23:19:15 +0800

    add 添加分析代码  Changes to be committed: 	modified:   src/MiniProfilerDemo/Controllers/HomeController.cs

--- src/MiniProfilerDemo/Controllers/HomeController.cs
+++ src/MiniProfilerDemo/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MiniProfilerDemo.Models;
+using StackExchange.Profiling;
 
 namespace MiniProfilerDemo.Controllers
 {
@@ -12,18 +13,29 @@ namespace MiniProfilerDemo.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // 分析代码段
+            return MiniProfiler.Current.Inline<IActionResult>(() => { return View(); }, "第1.1步");
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            // 分析代码段 多层嵌套
+            using (MiniProfiler.Current.Step("第1.1步"))
+            {
+                Console.WriteLine("test");
+                return MiniProfiler.Current.Inline<IActionResult>(() => { return View(); }, "第1.1.1步");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // 自定义分析
+            using (CustomTiming customTiming = MiniProfiler.Current.CustomTiming(category: "http", commandString: string.Empty, executeType: "GET", includeStackTrace: true))
+            {
+                customTiming.CommandString = "vito-Test";
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
 }
