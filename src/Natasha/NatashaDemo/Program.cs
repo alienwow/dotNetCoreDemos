d5769bc34743cae727092882ea77675bcbc2a33d
Vito <wuwenhao0327@gmail.com>
2019-08-05 22:44:27 +0800

    demo: add natasha demo

---
+++ src/Natasha/NatashaDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Natasha;
+
+namespace NatashaDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            FirstAction();
+            await FirstAsyncAction();
+        }
+
+        static void FirstAction()
+        {
+            var action = FastMethodOperator.New
+                .Param<string>("str1")
+                .Param(typeof(string), "str2")
+                .MethodBody("return str1+str2;")
+                .Return<string>()
+                .Complie<Func<string, string, string>>();
+
+            var result = action("Hello ", "World!");
+            Console.WriteLine(result);
+        }
+
+        static async Task FirstAsyncAction()
+        {
+            var action = FastMethodOperator.New
+                .UseAsync()
+                .Param<string>("str1")
+                .Param(typeof(string), "str2")
+                .MethodBody("return str1+str2;")
+                .Return<Task<string>>()
+                .Complie<Func<string, string, Task<string>>>();
+
+            var result = await action("Hello ", "Async World!");
+            Console.WriteLine(result);
+        }
+    }
+}
