920bdadbcdb519fd6cf8f2163e6afd12c08fd82b
Vito.Wu <wuwenhao0327@gmail.com>
2020-03-22 00:54:50 +0800

    demos: add natasha

---
+++ src/Natasha/NatashaMvc3.1Demo/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using NatashaMvc3._1Demo.Models;
+
+//namespace NatashaMvc3._1Demo.Controllers
+//{
+//    public class TestController : Controller
+//    {
+//        private readonly ILogger<TestController> _logger;
+
+//        public TestController(ILogger<TestController> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
---
+++ src/Natasha/NatashaMvc3.1Demo/Startup.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Natasha;
+
+namespace NatashaMvc3._1Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .ConfigureApplicationPartManager(appManager =>
+                {
+                    // var domain = NDomain.Create(DomainManagment.CurrentDomain);
+                    // var type1 = domain.GetType(ControllerTest0);
+                    // var type2 = domain.GetType(ControllerTest1);
+
+                    AssemblyComplier oop = new AssemblyComplier();
+                    oop.Add(ControllerTest0);
+                    Type type1 = oop.GetType("TestController");
+
+                    var feature = new ControllerFeature();
+                    appManager.ApplicationParts.Add(new AssemblyPart(type1.Assembly));
+                    //appManager.ApplicationParts.Add(new AssemblyPart(type2.Assembly));
+                    appManager.PopulateFeature(feature);
+                });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+        private readonly string ControllerTest0 = @"
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NatashaMvc3._1Demo.Models;
+
+namespace NatashaMvc3._1Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+";
+        private readonly string ControllerTest1 = @"
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NatashaWebDemo.Controllers
+{
+    [Route(""api/[controller]"")]
+    [ApiController]
+    public class Test2Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { ""value1"", ""value2"" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut(""{id}"")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete(""{id}"")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+";
+    }
+}
